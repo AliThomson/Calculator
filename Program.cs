a1127a321a784d010748344a2e7127829041a5af
Alison Thomson <alisonmcknight@hotmail.com>
2022-04-13 15:47:27 +0100

    Add if statement

--- Program.cs
+++ Program.cs
@@ -4,15 +4,40 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("enter an operator: ");
+            string operand = Console.ReadLine();
+                        
             Console.WriteLine("enter a number: ");
             string firstString = Console.ReadLine();
             int firstNumber = int.Parse(firstString);
+
             Console.WriteLine("and another");
             string secondString = Console.ReadLine();
             int secondNumber = int.Parse(secondString);
-            int product = firstNumber * secondNumber;
-            Console.WriteLine("And the answer is: " + product);
-            Console.ReadLine();
+            int sum = 0;
+
+            if (operand == "+") 
+            {
+                sum = firstNumber + secondNumber;
+            } else if ( operand == "-")
+            {
+                sum = firstNumber - secondNumber;
+            } else if ( operand == "*")
+            {
+                sum = firstNumber * secondNumber;
+            } else if ( operand == "/")
+            {
+                sum = firstNumber / secondNumber;
+            } else
+            {
+                Console.WriteLine("Please enter a valid operand i.e. +, -. * or /");
+            }
+            if (sum != 0){
+                Console.WriteLine("And the answer is: " + sum);
+                Console.ReadLine();
+            }
+            
+            
         }
     }
 }
