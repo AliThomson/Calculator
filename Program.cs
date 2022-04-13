f6c6738bba2f5ff094b8e5da30fee26d8185eda2
Alison Thomson <alisonmcknight@hotmail.com>
2022-04-13 15:21:51 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a number: ");
+            string firstString = Console.ReadLine();
+            int firstNumber = int.Parse(firstString);
+            Console.WriteLine("and another");
+            string secondString = Console.ReadLine();
+            int secondNumber = int.Parse(secondString);
+            int product = firstNumber * secondNumber;
+            Console.WriteLine("And the answer is: " + product);
+            Console.ReadLine();
+        }
+    }
+}
